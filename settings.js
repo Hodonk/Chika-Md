023fa9eb25e92d495c1e50206dfec65f20c7cd9e
Hodonk <115161072+Hodonk@users.noreply.github.com>
2022-10-15 22:26:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6287787498986','6287787498986','6287787498986'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6283837709331'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6287787498986' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Fuad' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Fuad-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Fuad' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,7 +41,7 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = '© Fuad-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
